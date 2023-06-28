d73d0e96aec1c94e4734bb8ad1c8bb0bf89f854d
Kay <Kay@DESKTOP-6LM0MTN>
2023-06-28 03:11:23 +0000

    Add project files.

---
+++ SQL App/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqlapp.Models;
+using sqlapp.Services;
+
+namespace SQL_App.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+
+        }
+    }
+}
